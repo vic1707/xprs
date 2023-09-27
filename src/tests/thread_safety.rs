edfb71ef8e5db3d0759db3302145bae5dd85f635
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-09-27 11:13:48 +0200

    more checks on thread safety

--- src/tests/thread_safety.rs
+++ src/tests/thread_safety.rs
@@ -1,18 +1,24 @@
 /* Crate imports */
 use crate::{
     element::{BinOp, Element, FunctionCall, UnOp},
-    token::Operator,
-    Parser,
+    parser::{Error, ErrorKind, Parser},
+    token::{Identifier, Operator},
 };
 
 const fn is_sized_send_sync_unpin<T: Sized + Send + Sync + Unpin>() {}
 
 #[test]
 const fn test_thread_safety() {
-    is_sized_send_sync_unpin::<Parser>();
-    is_sized_send_sync_unpin::<Element<'_>>();
+    // element module
     is_sized_send_sync_unpin::<BinOp<'_>>();
-    is_sized_send_sync_unpin::<UnOp<'_>>();
+    is_sized_send_sync_unpin::<Element<'_>>();
     is_sized_send_sync_unpin::<FunctionCall<'_>>();
+    is_sized_send_sync_unpin::<UnOp<'_>>();
+    // parser module
+    is_sized_send_sync_unpin::<Error>();
+    is_sized_send_sync_unpin::<ErrorKind>();
+    is_sized_send_sync_unpin::<Parser>();
+    // token module
+    is_sized_send_sync_unpin::<Identifier>();
     is_sized_send_sync_unpin::<Operator>();
 }
