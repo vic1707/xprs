6249db8a72bf1aaf065a08c97f600ec9c8d835d3
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-10-13 10:48:00 +0200

    docs around valgrind sh

--- valgrind.sh
+++ valgrind.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+## Because valgrind is not supported on arm macOS
+## so we use docker to run valgrind on linux
+
 docker image rm -f valgrind
 docker build -t valgrind .
 docker run -it --rm -v $(pwd):/home/valgrind valgrind sh -c "\
