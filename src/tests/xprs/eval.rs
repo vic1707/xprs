1cfeba69b42edf8aa2cf633976e25c796257de98
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-10-16 12:01:38 +0200

    fmt

--- src/tests/xprs/eval.rs
+++ src/tests/xprs/eval.rs
@@ -16,24 +16,33 @@ const ERROR_MARGIN: f64 = f64::EPSILON;
 /// PEMDAS vs PEJMDAS
 /// 6/2(2+1)
 /// 1/2x
-fn get_valid_test_cases() -> [(&'static str, HashMap<&'static str, f64>, f64); 10] {
+fn get_valid_test_cases(
+) -> [(&'static str, HashMap<&'static str, f64>, f64); 10] {
     [
         ("2(3)", [].into(), 6.0_f64),
         ("2 + 3 ^ 2 * 3 + 4", [].into(), 33.0_f64),
         ("2^2^(2^2 + 1)", [].into(), 1024.0_f64), // MacOS says it's 4_294_967_296.0_f64
-        ("2 * x + 3y + 4x + 5", [("x", 2.0_f64), ("y", 3.0_f64)].into(), 26.0_f64),
+        (
+            "2 * x + 3y + 4x + 5",
+            [("x", 2.0_f64), ("y", 3.0_f64)].into(),
+            26.0_f64,
+        ),
         ("sin(-cos(2))", [].into(), 0.404_239_153_852_265_8_f64),
         ("sin(2)^2", [].into(), 0.826_821_810_431_806_f64),
-        ("12 + 3-1x+3y x", [("x", 2.0_f64), ("y", 3.0_f64)].into(), 31.0_f64),
+        (
+            "12 + 3-1x+3y x",
+            [("x", 2.0_f64), ("y", 3.0_f64)].into(),
+            31.0_f64,
+        ),
         ("2 (3+4) 5", [].into(), 70.0_f64),
         #[cfg(feature = "pemdas")]
-        ("6/2(2+1)", [].into(), 9.0_f64),               // is "6/2*(2+1)"
+        ("6/2(2+1)", [].into(), 9.0_f64), // is "6/2*(2+1)"
         #[cfg(feature = "pejmdas")]
-        ("6/2(2+1)", [].into(), 1.0_f64),               // is "6/(2*(2+1))"
+        ("6/2(2+1)", [].into(), 1.0_f64), // is "6/(2*(2+1))"
         #[cfg(feature = "pemdas")]
-        ("1/2x", [("x", 2.0_f64)].into(), 1_f64),       // is "(1/2)*x"
+        ("1/2x", [("x", 2.0_f64)].into(), 1_f64), // is "(1/2)*x"
         #[cfg(feature = "pejmdas")]
-        ("1/2x", [("x", 2.0_f64)].into(), 0.25_f64),    // is "1/(2*x)"
+        ("1/2x", [("x", 2.0_f64)].into(), 0.25_f64), // is "1/(2*x)"
     ]
 }
 
@@ -45,6 +54,12 @@ fn test_eval() {
         let xprs = parser.parse(input);
         assert!(xprs.is_ok());
         let result = xprs.unwrap().eval(&vars);
-        assert!((result.unwrap() - expected).abs() < ERROR_MARGIN, "{}\nExpected: {}, got: {}", input, expected, result.unwrap());
+        assert!(
+            (result.unwrap() - expected).abs() < ERROR_MARGIN,
+            "{}\nExpected: {}, got: {}",
+            input,
+            expected,
+            result.unwrap()
+        );
     }
 }
