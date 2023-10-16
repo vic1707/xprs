4e92291177f66700e215cbb23780d978d4a845c1
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-10-16 10:56:19 +0200

    fix tests + add for variables

--- src/tests/parser/valid.rs
+++ src/tests/parser/valid.rs
@@ -4,7 +4,7 @@ use core::f64;
 use crate::{
     element::{BinOp, Element, FunctionCall, UnOp},
     token::Operator,
-    Parser,
+    Parser, Xprs,
 };
 
 ///// Tests the following expressions:
@@ -28,232 +28,330 @@ use crate::{
 /// abs(sin(2))
 /// sin(-cos(2))
 /// sin(2)^2
+/// 2 * x + 3y + 4x + 5
 #[allow(clippy::too_many_lines)]
-fn get_valid_test_cases<'a>() -> Vec<(&'static str, Element<'a>)> {
+fn get_valid_test_cases<'a>() -> Vec<(&'static str, Xprs<'a>)> {
     vec![
-        ("2", Element::Number(2.0)),
-        ("((2))", Element::Number(2.0)),
+        (
+            "2",
+            Xprs {
+                root: Element::Number(2.0),
+                vars: [].into(),
+            },
+        ),
+        (
+            "((2))",
+            Xprs {
+                root: Element::Number(2.0),
+                vars: [].into(),
+            },
+        ),
         (
             "2 + pi",
-            Element::BinOp(Box::new(BinOp::new(
-                Operator::Plus,
-                Element::Number(2.0),
-                Element::Number(f64::consts::PI),
-            ))),
+            Xprs {
+                root: Element::BinOp(Box::new(BinOp::new(
+                    Operator::Plus,
+                    Element::Number(2.0),
+                    Element::Number(f64::consts::PI),
+                ))),
+                vars: [].into(),
+            },
         ),
         (
             "2 * y",
-            Element::BinOp(Box::new(BinOp::new(
-                Operator::Times,
-                Element::Number(2.0),
-                Element::Variable("y"),
-            ))),
+            Xprs {
+                root: Element::BinOp(Box::new(BinOp::new(
+                    Operator::Times,
+                    Element::Number(2.0),
+                    Element::Variable("y"),
+                ))),
+                vars: ["y"].into(),
+            },
         ),
         (
             "(2 + 1) + 3",
-            Element::BinOp(Box::new(BinOp::new(
-                Operator::Plus,
-                Element::BinOp(Box::new(BinOp::new(
+            Xprs {
+                root: Element::BinOp(Box::new(BinOp::new(
                     Operator::Plus,
-                    Element::Number(2.0),
-                    Element::Number(1.0),
+                    Element::BinOp(Box::new(BinOp::new(
+                        Operator::Plus,
+                        Element::Number(2.0),
+                        Element::Number(1.0),
+                    ))),
+                    Element::Number(3.0),
                 ))),
-                Element::Number(3.0),
-            ))),
+                vars: [].into(),
+            },
         ),
         (
             "-(2 + 1)^4",
-            Element::UnOp(Box::new(UnOp::new(
-                Operator::Minus,
-                Element::BinOp(Box::new(BinOp::new(
-                    Operator::Power,
+            Xprs {
+                root: Element::UnOp(Box::new(UnOp::new(
+                    Operator::Minus,
                     Element::BinOp(Box::new(BinOp::new(
-                        Operator::Plus,
-                        Element::Number(2.0),
-                        Element::Number(1.0),
+                        Operator::Power,
+                        Element::BinOp(Box::new(BinOp::new(
+                            Operator::Plus,
+                            Element::Number(2.0),
+                            Element::Number(1.0),
+                        ))),
+                        Element::Number(4.0),
                     ))),
-                    Element::Number(4.0),
                 ))),
-            ))),
+                vars: [].into(),
+            },
         ),
         (
             "(-(2 + 1))^4",
-            Element::BinOp(Box::new(BinOp::new(
-                Operator::Power,
-                Element::UnOp(Box::new(UnOp::new(
-                    Operator::Minus,
-                    Element::BinOp(Box::new(BinOp::new(
-                        Operator::Plus,
-                        Element::Number(2.0),
-                        Element::Number(1.0),
+            Xprs {
+                root: Element::BinOp(Box::new(BinOp::new(
+                    Operator::Power,
+                    Element::UnOp(Box::new(UnOp::new(
+                        Operator::Minus,
+                        Element::BinOp(Box::new(BinOp::new(
+                            Operator::Plus,
+                            Element::Number(2.0),
+                            Element::Number(1.0),
+                        ))),
                     ))),
+                    Element::Number(4.0),
                 ))),
-                Element::Number(4.0),
-            ))),
+                vars: [].into(),
+            },
         ),
         (
             "2 + 3",
-            Element::BinOp(Box::new(BinOp::new(
-                Operator::Plus,
-                Element::Number(2.0),
-                Element::Number(3.0),
-            ))),
+            Xprs {
+                root: Element::BinOp(Box::new(BinOp::new(
+                    Operator::Plus,
+                    Element::Number(2.0),
+                    Element::Number(3.0),
+                ))),
+                vars: [].into(),
+            },
         ),
         (
             "2 + 3 * 4",
-            Element::BinOp(Box::new(BinOp::new(
-                Operator::Plus,
-                Element::Number(2.0),
-                Element::BinOp(Box::new(BinOp::new(
-                    Operator::Times,
-                    Element::Number(3.0),
-                    Element::Number(4.0),
-                ))),
-            ))),
-        ),
-        (
-            "2 * 4 + 1",
-            Element::BinOp(Box::new(BinOp::new(
-                Operator::Plus,
-                Element::BinOp(Box::new(BinOp::new(
-                    Operator::Times,
-                    Element::Number(2.0),
-                    Element::Number(4.0),
-                ))),
-                Element::Number(1.0),
-            ))),
-        ),
-        (
-            "3 / 2 * 4",
-            Element::BinOp(Box::new(BinOp::new(
-                Operator::Times,
-                Element::BinOp(Box::new(BinOp::new(
-                    Operator::Divide,
-                    Element::Number(3.0),
-                    Element::Number(2.0),
-                ))),
-                Element::Number(4.0),
-            ))),
-        ),
-        (
-            "3 % 2 * 4",
-            Element::BinOp(Box::new(BinOp::new(
-                Operator::Times,
-                Element::BinOp(Box::new(BinOp::new(
-                    Operator::Modulo,
-                    Element::Number(3.0),
-                    Element::Number(2.0),
-                ))),
-                Element::Number(4.0),
-            ))),
-        ),
-        (
-            "2 + -5",
-            Element::BinOp(Box::new(BinOp::new(
-                Operator::Plus,
-                Element::Number(2.0),
-                Element::UnOp(Box::new(UnOp::new(
-                    Operator::Minus,
-                    Element::Number(5.0),
-                ))),
-            ))),
-        ),
-        (
-            "2 + 3 ^ 2 * 3 + 4",
-            Element::BinOp(Box::new(BinOp::new(
-                Operator::Plus,
-                Element::BinOp(Box::new(BinOp::new(
+            Xprs {
+                root: Element::BinOp(Box::new(BinOp::new(
                     Operator::Plus,
                     Element::Number(2.0),
                     Element::BinOp(Box::new(BinOp::new(
                         Operator::Times,
-                        Element::BinOp(Box::new(BinOp::new(
-                            Operator::Power,
-                            Element::Number(3.0),
-                            Element::Number(2.0),
-                        ))),
                         Element::Number(3.0),
+                        Element::Number(4.0),
                     ))),
                 ))),
-                Element::Number(4.0),
-            ))),
+                vars: [].into(),
+            },
+        ),
+        (
+            "2 * 4 + 1",
+            Xprs {
+                root: Element::BinOp(Box::new(BinOp::new(
+                    Operator::Plus,
+                    Element::BinOp(Box::new(BinOp::new(
+                        Operator::Times,
+                        Element::Number(2.0),
+                        Element::Number(4.0),
+                    ))),
+                    Element::Number(1.0),
+                ))),
+                vars: [].into(),
+            },
+        ),
+        (
+            "3 / 2 * 4",
+            Xprs {
+                root: Element::BinOp(Box::new(BinOp::new(
+                    Operator::Times,
+                    Element::BinOp(Box::new(BinOp::new(
+                        Operator::Divide,
+                        Element::Number(3.0),
+                        Element::Number(2.0),
+                    ))),
+                    Element::Number(4.0),
+                ))),
+                vars: [].into(),
+            },
+        ),
+        (
+            "3 % 2 * 4",
+            Xprs {
+                root: Element::BinOp(Box::new(BinOp::new(
+                    Operator::Times,
+                    Element::BinOp(Box::new(BinOp::new(
+                        Operator::Modulo,
+                        Element::Number(3.0),
+                        Element::Number(2.0),
+                    ))),
+                    Element::Number(4.0),
+                ))),
+                vars: [].into(),
+            },
+        ),
+        (
+            "2 + -5",
+            Xprs {
+                root: Element::BinOp(Box::new(BinOp::new(
+                    Operator::Plus,
+                    Element::Number(2.0),
+                    Element::UnOp(Box::new(UnOp::new(
+                        Operator::Minus,
+                        Element::Number(5.0),
+                    ))),
+                ))),
+                vars: [].into(),
+            },
+        ),
+        (
+            "2 + 3 ^ 2 * 3 + 4",
+            Xprs {
+                root: Element::BinOp(Box::new(BinOp::new(
+                    Operator::Plus,
+                    Element::BinOp(Box::new(BinOp::new(
+                        Operator::Plus,
+                        Element::Number(2.0),
+                        Element::BinOp(Box::new(BinOp::new(
+                            Operator::Times,
+                            Element::BinOp(Box::new(BinOp::new(
+                                Operator::Power,
+                                Element::Number(3.0),
+                                Element::Number(2.0),
+                            ))),
+                            Element::Number(3.0),
+                        ))),
+                    ))),
+                    Element::Number(4.0),
+                ))),
+                vars: [].into(),
+            },
         ),
         (
             "2^2^(2^2 + 1)",
-            Element::BinOp(Box::new(BinOp::new(
-                Operator::Power,
-                Element::BinOp(Box::new(BinOp::new(
+            Xprs {
+                root: Element::BinOp(Box::new(BinOp::new(
                     Operator::Power,
-                    Element::Number(2.0),
-                    Element::Number(2.0),
-                ))),
-                Element::BinOp(Box::new(BinOp::new(
-                    Operator::Plus,
                     Element::BinOp(Box::new(BinOp::new(
                         Operator::Power,
                         Element::Number(2.0),
                         Element::Number(2.0),
                     ))),
-                    Element::Number(1.0),
-                ))),
-            ))),
-        ),
-        (
-            "2 * (3 + (4 - 1))",
-            Element::BinOp(Box::new(BinOp::new(
-                Operator::Times,
-                Element::Number(2.0),
-                Element::BinOp(Box::new(BinOp::new(
-                    Operator::Plus,
-                    Element::Number(3.0),
                     Element::BinOp(Box::new(BinOp::new(
-                        Operator::Minus,
-                        Element::Number(4.0),
+                        Operator::Plus,
+                        Element::BinOp(Box::new(BinOp::new(
+                            Operator::Power,
+                            Element::Number(2.0),
+                            Element::Number(2.0),
+                        ))),
                         Element::Number(1.0),
                     ))),
                 ))),
-            ))),
+                vars: [].into(),
+            },
+        ),
+        (
+            "2 * (3 + (4 - 1))",
+            Xprs {
+                root: Element::BinOp(Box::new(BinOp::new(
+                    Operator::Times,
+                    Element::Number(2.0),
+                    Element::BinOp(Box::new(BinOp::new(
+                        Operator::Plus,
+                        Element::Number(3.0),
+                        Element::BinOp(Box::new(BinOp::new(
+                            Operator::Minus,
+                            Element::Number(4.0),
+                            Element::Number(1.0),
+                        ))),
+                    ))),
+                ))),
+                vars: [].into(),
+            },
         ),
         (
             "sin(2)",
-            Element::Function(Box::new(FunctionCall::new(
-                f64::sin,
-                Element::Number(2.),
-            ))),
+            Xprs {
+                root: Element::Function(Box::new(FunctionCall::new(
+                    f64::sin,
+                    Element::Number(2.),
+                ))),
+                vars: [].into(),
+            },
         ),
         (
             "abs(sin(2))",
-            Element::Function(Box::new(FunctionCall::new(
-                f64::abs,
-                Element::Function(Box::new(FunctionCall::new(
-                    f64::sin,
-                    Element::Number(2.),
-                ))),
-            ))),
-        ),
-        (
-            "sin(-cos(2))",
-            Element::Function(Box::new(FunctionCall::new(
-                f64::sin,
-                Element::UnOp(Box::new(UnOp::new(
-                    Operator::Minus,
+            Xprs {
+                root: Element::Function(Box::new(FunctionCall::new(
+                    f64::abs,
                     Element::Function(Box::new(FunctionCall::new(
-                        f64::cos,
+                        f64::sin,
                         Element::Number(2.),
                     ))),
                 ))),
-            ))),
+                vars: [].into(),
+            },
+        ),
+        (
+            "sin(-cos(2))",
+            Xprs {
+                root: Element::Function(Box::new(FunctionCall::new(
+                    f64::sin,
+                    Element::UnOp(Box::new(UnOp::new(
+                        Operator::Minus,
+                        Element::Function(Box::new(FunctionCall::new(
+                            f64::cos,
+                            Element::Number(2.),
+                        ))),
+                    ))),
+                ))),
+                vars: [].into(),
+            },
         ),
         (
             "sin(2)^2",
-            Element::BinOp(Box::new(BinOp::new(
-                Operator::Power,
-                Element::Function(Box::new(FunctionCall::new(
-                    f64::sin,
+            Xprs {
+                root: Element::BinOp(Box::new(BinOp::new(
+                    Operator::Power,
+                    Element::Function(Box::new(FunctionCall::new(
+                        f64::sin,
+                        Element::Number(2.),
+                    ))),
                     Element::Number(2.),
                 ))),
-                Element::Number(2.),
-            ))),
+                vars: [].into(),
+            },
+        ),
+        (
+            "2 * x + 3y + 4x + 5",
+            Xprs {
+                root: Element::BinOp(Box::new(BinOp::new(
+                    Operator::Plus,
+                    Element::BinOp(Box::new(BinOp::new(
+                        Operator::Plus,
+                        Element::BinOp(Box::new(BinOp::new(
+                            Operator::Plus,
+                            Element::BinOp(Box::new(BinOp::new(
+                                Operator::Times,
+                                Element::Number(2.),
+                                Element::Variable("x"),
+                            ))),
+                            Element::BinOp(Box::new(BinOp::new(
+                                Operator::Times,
+                                Element::Number(3.),
+                                Element::Variable("y"),
+                            ))),
+                        ))),
+                        Element::BinOp(Box::new(BinOp::new(
+                            Operator::Times,
+                            Element::Number(4.),
+                            Element::Variable("x"),
+                        ))),
+                    ))),
+                    Element::Number(5.),
+                ))),
+                vars: ["x", "y"].into(),
+            },
         ),
     ]
 }
@@ -264,7 +362,7 @@ fn test_valid() {
     for (expr, expected) in get_valid_test_cases() {
         let res = parser.parse(expr);
         assert!(res.is_ok(), "\nShould have passed for {expr}\n{res:?}");
-        assert_eq!(res.unwrap().root, expected, "\n{expr}");
+        assert_eq!(res.unwrap(), expected, "\n{expr}");
     }
 }
 
