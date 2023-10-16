e37f403b45bef15750e69e3bb49bbde1144d5cb2
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-10-16 17:45:50 +0200

    bind API for `xprs`

--- src/xprs.rs
+++ src/xprs.rs
@@ -99,3 +99,66 @@ impl XprsImpl<'_> {
 #[derive(Debug, Eq, PartialEq, thiserror::Error)]
 #[error("Evaluation error: '{0}' was not provided")]
 pub struct EvalError(String);
+
+//////////////////////////////////////////////////////////////////////////////
+//  TODO: replace this with variadic generics when it's available & stable  //
+//////////////////////////////////////////////////////////////////////////////
+#[allow(clippy::too_many_arguments)]
+#[rustfmt::skip]
+impl<'a> Xprs<'a> {
+    #[inline]
+    pub fn bind(self, var: &'a str) -> impl Fn(f64) -> Result<f64, EvalError> + 'a {
+        move |val| self.eval(&[(var, val)].into())
+    }
+
+    #[inline]
+    pub fn bind2(self, var1: &'a str, var2: &'a str) -> impl Fn(f64, f64) -> Result<f64, EvalError> + 'a {
+        move |val1, val2| self.eval(&[(var1, val1), (var2, val2)].into())
+    }
+
+    #[inline]
+    pub fn bind3(self, var1: &'a str, var2: &'a str, var3: &'a str) -> impl Fn(f64, f64, f64) -> Result<f64, EvalError> + 'a {
+        move |val1, val2, val3| self.eval(&[(var1, val1), (var2, val2), (var3, val3)].into())
+    }
+
+    #[inline]
+    pub fn bind4(self, var1: &'a str, var2: &'a str, var3: &'a str, var4: &'a str) -> impl Fn(f64, f64, f64, f64) -> Result<f64, EvalError> + 'a {
+        move |val1, val2, val3, val4| self.eval(&[(var1, val1), (var2, val2), (var3, val3), (var4, val4)].into())
+    }
+
+    #[inline]
+    pub fn bind5(self, var1: &'a str, var2: &'a str, var3: &'a str, var4: &'a str, var5: &'a str) -> impl Fn(f64, f64, f64, f64, f64) -> Result<f64, EvalError> + 'a {
+        move |val1, val2, val3, val4, val5| self.eval(&[(var1, val1), (var2, val2), (var3, val3), (var4, val4), (var5, val5)].into())
+    }
+
+    #[inline]
+    pub fn bind6(self, var1: &'a str, var2: &'a str, var3: &'a str, var4: &'a str, var5: &'a str, var6: &'a str) -> impl Fn(f64, f64, f64, f64, f64, f64) -> Result<f64, EvalError> + 'a {
+        move |val1, val2, val3, val4, val5, val6| self.eval(&[(var1, val1), (var2, val2), (var3, val3), (var4, val4), (var5, val5), (var6, val6)].into())
+    }
+
+    #[inline]
+    pub fn bind7(self, var1: &'a str, var2: &'a str, var3: &'a str, var4: &'a str, var5: &'a str, var6: &'a str, var7: &'a str) -> impl Fn(f64, f64, f64, f64, f64, f64, f64) -> Result<f64, EvalError> + 'a {
+        move |val1, val2, val3, val4, val5, val6, val7| self.eval(&[(var1, val1), (var2, val2), (var3, val3), (var4, val4), (var5, val5), (var6, val6), (var7, val7)].into())
+    }
+
+    #[inline]
+    pub fn bind8(self, var1: &'a str, var2: &'a str, var3: &'a str, var4: &'a str, var5: &'a str, var6: &'a str, var7: &'a str, var8: &'a str) -> impl Fn(f64, f64, f64, f64, f64, f64, f64, f64) -> Result<f64, EvalError> + 'a {
+        move |val1, val2, val3, val4, val5, val6, val7, val8| self.eval(&[(var1, val1), (var2, val2), (var3, val3), (var4, val4), (var5, val5), (var6, val6), (var7, val7), (var8, val8)].into())
+    }
+
+    #[inline]
+    pub fn bind9(self, var1: &'a str, var2: &'a str, var3: &'a str, var4: &'a str, var5: &'a str, var6: &'a str, var7: &'a str, var8: &'a str, var9: &'a str) -> impl Fn(f64, f64, f64, f64, f64, f64, f64, f64, f64) -> Result<f64, EvalError> + 'a {
+        move |val1, val2, val3, val4, val5, val6, val7, val8, val9| self.eval(&[(var1, val1), (var2, val2), (var3, val3), (var4, val4), (var5, val5), (var6, val6), (var7, val7), (var8, val8), (var9, val9)].into())
+    }
+
+    // NOTE: Too lazy to implement this for more than 9 variables even with Copilot
+    // + I don't really think anyone will need more than 9 variables anyway
+    #[inline]
+    pub fn bind_n<const T: usize>(self, vars: [&'a str; T]) -> impl Fn([f64; T]) -> Result<f64, EvalError> + 'a {
+        move |vals| self.eval(&vars.iter().copied().zip(vals).collect())
+    }
+    #[inline]
+    pub fn bind_n_runtime(self, vars: &'a [&'a str]) -> impl Fn(&[f64]) -> Result<f64, EvalError> + 'a {
+        move |vals| self.eval(&vars.iter().copied().zip(vals.iter().copied()).collect())
+    }
+}
