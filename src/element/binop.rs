19b23a880be364eaf9787ec4df81dd890462a3d4
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-10-19 22:18:15 +0200

    PartialEq for BinOp to benefit from commutativity

--- src/element/binop.rs
+++ src/element/binop.rs
@@ -4,7 +4,7 @@ use core::fmt;
 use super::Element;
 use crate::token::Operator;
 
-#[derive(Debug, PartialEq, PartialOrd)]
+#[derive(Debug, PartialOrd)]
 pub struct BinOp<'a> {
     pub(crate) op: Operator,
     pub(crate) lhs: Element<'a>,
@@ -20,5 +20,26 @@ impl<'a> BinOp<'a> {
 impl fmt::Display for BinOp<'_> {
     fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(fmt, "({} {} {})", self.lhs, self.op, self.rhs)
+    }
+}
+
+#[allow(clippy::missing_trait_methods)]
+impl PartialEq for BinOp<'_> {
+    fn eq(&self, other: &Self) -> bool {
+        self.op == other.op
+            && match self.op {
+                // non commutative operators
+                Operator::Divide
+                | Operator::Minus
+                | Operator::Modulo
+                | Operator::Power => {
+                    self.lhs == other.lhs && self.rhs == other.rhs
+                },
+                // commutative operators
+                Operator::Plus | Operator::Times => {
+                    (self.lhs == other.lhs && self.rhs == other.rhs)
+                        || (self.lhs == other.rhs && self.rhs == other.lhs)
+                },
+            }
     }
 }
