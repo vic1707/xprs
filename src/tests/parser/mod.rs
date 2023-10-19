f56e9e9b876e7e91005899624d5a2f3a0b97c7c0
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-10-19 11:40:35 +0200

    fmt

--- src/tests/parser/mod.rs
+++ src/tests/parser/mod.rs
@@ -1,9 +1,9 @@
 /* Modules */
+#[cfg(feature = "compile-time-optimizations")]
+mod comptime_optimizations;
 #[cfg(not(feature = "compile-time-optimizations"))]
 mod ctx;
 mod error;
 mod implicit_multiplication;
 #[cfg(not(feature = "compile-time-optimizations"))]
 mod valid;
-#[cfg(feature = "compile-time-optimizations")]
-mod comptime_optimizations;
