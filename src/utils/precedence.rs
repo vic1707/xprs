d525c2b9515df3fdbe2d4e074d621a81fbe8df64
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-10-20 13:16:00 +0200

    fmt

--- src/utils/precedence.rs
+++ src/utils/precedence.rs
@@ -1,27 +1,27 @@
-    /* Crate imports */
-    use crate::token::Operator;
+/* Crate imports */
+use crate::token::Operator;
 
-    pub const NO_PRECEDENCE: usize = 0;
-    /* Unary operators */
-    pub const UNOP_PRECEDENCE: usize = 3;
-    /* Binary operators */
+pub const NO_PRECEDENCE: usize = 0;
+/* Unary operators */
+pub const UNOP_PRECEDENCE: usize = 3;
+/* Binary operators */
 
-    #[cfg(feature = "pejmdas")]
-    pub const IMPLICIT_MULTIPLICATION_PRECEDENCE: usize = 3;
-    #[cfg(feature = "pemdas")]
-    pub const IMPLICIT_MULTIPLICATION_PRECEDENCE: usize =
-        get_for_op(&Operator::Times);
+#[cfg(feature = "pejmdas")]
+pub const IMPLICIT_MULTIPLICATION_PRECEDENCE: usize = 3;
+#[cfg(feature = "pemdas")]
+pub const IMPLICIT_MULTIPLICATION_PRECEDENCE: usize =
+    get_for_op(&Operator::Times);
 
-    pub const IMPLICIT_MULTIPLICATION_INFO: (Operator, usize) =
-        (Operator::Times, IMPLICIT_MULTIPLICATION_PRECEDENCE);
+pub const IMPLICIT_MULTIPLICATION_INFO: (Operator, usize) =
+    (Operator::Times, IMPLICIT_MULTIPLICATION_PRECEDENCE);
 
-    pub const fn get_for_op(op: &Operator) -> usize {
-        match *op {
-            Operator::Plus | Operator::Minus => 1,
-            Operator::Times | Operator::Divide | Operator::Modulo => 2,
-            // uses `4` because `pejmdas` feature uses `3`
-            Operator::Power => 4,
-        }
+pub const fn get_for_op(op: &Operator) -> usize {
+    match *op {
+        Operator::Plus | Operator::Minus => 1,
+        Operator::Times | Operator::Divide | Operator::Modulo => 2,
+        // uses `4` because `pejmdas` feature uses `3`
+        Operator::Power => 4,
     }
-    /* Functions */
-    pub const FN_PRECEDENCE: usize = 5;
+}
+/* Functions */
+pub const FN_PRECEDENCE: usize = 5;
