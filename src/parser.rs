c116947acd2726a962b265cd2333f07997e42656
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-10-27 23:16:24 +0200

    fix clippy

--- src/parser.rs
+++ src/parser.rs
@@ -3,9 +3,11 @@
 /* Built-in imports */
 use core::str;
 /* Crate imports */
+#[cfg(feature = "compile-time-optimizations")]
+use crate::element::Simplify;
 use crate::{
     context::Context,
-    element::{BinOp, Element, FunctionCall, Simplify, UnOp},
+    element::{BinOp, Element, FunctionCall, UnOp},
     misc::HashSet,
     token::{Identifier, Operator},
     utils::{
