12b78087b1fe485bfa26dc9fd1ee56be17167f38
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-10-31 15:04:02 +0100

    fix `xprs_fn!` macro usage

--- src/token/function.rs
+++ src/token/function.rs
@@ -25,24 +25,65 @@ impl<'a> Function<'a> {
 macro_rules! xprs_fn {
     // variadics
     ($name:literal, $function:expr) => {
-        $crate::token::Function::new($name, $function, None)
+        $crate::Function::new($name, $function, None)
     };
     ($function:expr) => {
-        $crate::token::Function::new(stringify!($function), $function, None)
+        $crate::Function::new(stringify!($function), $function, None)
     };
     // fixed args
     ($name:literal, $function:expr, $nb_args:tt) => {
-        $crate::token::Function::new(
+        $crate::Function::new(
             $name,
-            $crate::utils::macros::wrap_into_closure!($function, $nb_args),
+            $crate::xprs_fn!(wrap $function, $nb_args),
             Some($nb_args),
         )
     };
     ($function:expr, $nb_args:tt) => {
-        $crate::token::Function::new(
+        $crate::Function::new(
             stringify!($function),
-            $crate::utils::macros::wrap_into_closure!($function, $nb_args),
+            $crate::xprs_fn!(wrap $function, $nb_args),
             Some($nb_args),
         )
+    };
+
+    //// closure wraping ////
+    (wrap $function:expr, 0) => {
+        move |_| $function()
+    };
+    (wrap $function:expr, 1) => {
+        #[allow(clippy::indexing_slicing)]
+        move |args| $function(args[0])
+    };
+    (wrap $function:expr, 2) => {
+        #[allow(clippy::indexing_slicing)]
+        move |args| $function(args[0], args[1])
+    };
+    (wrap $function:expr, 3) => {
+        #[allow(clippy::indexing_slicing)]
+        move |args| $function(args[0], args[1], args[2])
+    };
+    (wrap $function:expr, 4) => {
+        #[allow(clippy::indexing_slicing)]
+        move |args| $function(args[0], args[1], args[2], args[3])
+    };
+    (wrap $function:expr, 5) => {
+        #[allow(clippy::indexing_slicing)]
+        move |args| $function(args[0], args[1], args[2], args[3], args[4])
+    };
+    (wrap $function:expr, 6) => {
+        #[allow(clippy::indexing_slicing)]
+        move |args| $function(args[0], args[1], args[2], args[3], args[4], args[5])
+    };
+    (wrap $function:expr, 7) => {
+        #[allow(clippy::indexing_slicing)]
+        move |args| $function(args[0], args[1], args[2], args[3], args[4], args[5], args[6])
+    };
+    (wrap $function:expr, 8) => {
+        #[allow(clippy::indexing_slicing)]
+        move |args| $function(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7])
+    };
+    (wrap $function:expr, 9) => {
+        #[allow(clippy::indexing_slicing)]
+        move |args| $function(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8])
     };
 }
