8367784e9f4825104666a853a950026a359998a6
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-10-31 13:11:42 +0100

    built-in functions probably don't need to be exported

--- src/token/mod.rs
+++ src/token/mod.rs
@@ -3,6 +3,6 @@ mod function;
 mod identifier;
 mod operator;
 /* Exports */
-pub use function::{built_in_functions, Function};
+pub use function::Function;
 pub use identifier::Identifier;
 pub use operator::Operator;
