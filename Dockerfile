894803b5d7403c4afcc2165a0ac653fd1dc24401
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-11-27 19:43:33 +0100

    rework docker valgrind

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,8 @@
 FROM alpine:latest
 RUN apk add valgrind cargo
+RUN cargo install cargo-valgrind
+
+WORKDIR /home/valgrind
+
+# CMD [ "cargo", "valgrind", "run"]
+CMD [ "cargo", "valgrind", "test"]
--- valgrind.sh
+++ valgrind.sh
@@ -5,13 +5,4 @@
 
 docker image rm -f valgrind
 docker build -t valgrind .
-docker run -it --rm -v $(pwd):/home/valgrind valgrind sh -c "\
-    cd /home/valgrind && \
-    cargo build --release && \
-    valgrind \
-        --leak-check=full \
-        --show-leak-kinds=all \
-        --track-origins=yes \
-        --verbose \
-        --log-file=valgrind-out.txt \
-        ./target/release/repl"
+docker run -it --rm -v $(pwd):/home/valgrind valgrind
