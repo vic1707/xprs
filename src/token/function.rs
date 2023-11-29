9ac627eee1148ed5314971f3ff62e8e1f037dce8
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-11-29 11:35:11 +0100

    fox doc links

--- src/token/function.rs
+++ src/token/function.rs
@@ -15,7 +15,7 @@ impl Function {
     /// Creates a new [`Function`] from the function components.
     /// Note that the fn pointer must be a function that takes a slice of f64 as argument and returns a f64.
     /// So make sure to wrap your function in a closure if it doesn't match the signature.
-    /// For convenience, you can use the [`xprs_fn!`] macro.
+    /// For convenience, you can use the [`crate::xprs_fn!`] macro.
     ///
     /// [`Function`] needs a fn taking a slice because Rust variadics are not available yet.
     #[inline]
@@ -33,7 +33,7 @@ impl Function {
 }
 
 /// Macro for defining functions for xprs' context easily, with optional variadic support.
-/// This macro is provided for convenience, since [`Function`] needs a fn taking a slice of [`f64`] as argument.
+/// This macro is provided for convenience, since [`crate::Function`] needs a fn taking a slice of [`f64`] as argument.
 /// The macro will wrap your function in a closure depending on the number of arguments you provide.
 ///
 /// Don't provide the number of arguments if your function is variadic (takes any number of arguments).
